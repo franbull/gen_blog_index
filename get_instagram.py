bfca235c4446b7f2e23ba1e50faa402fdcd8154c
Francis Bull <fbull@artlogic.com>
2014-04-01 10:37:25 -0700

    Fix exception when media.caption is None.

--- get_instagram.py
+++ get_instagram.py
@@ -7,7 +7,10 @@ def get_photos():
     api = InstagramAPI(access_token=access_token[0])
     recent_media, next = api.user_recent_media(user_id=access_token[1]['id'], count=10)
     for media in recent_media:
-        photos.append([media.images['standard_resolution'].url, media.caption.text])
+        caption_text = ''
+        if media.caption is not None:
+            caption_text = media.caption.text
+        photos.append([media.images['standard_resolution'].url, caption_text])
     return photos
 
 if __name__ == '__main__':
