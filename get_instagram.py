fa4f98bba3b0ad80ccdf533d6be883e060e83032
Francis Bull <fran@trello.com>
2015-03-19 17:33:37 -0700

    Update template, update get_instagram.

--- get_instagram.py
+++ get_instagram.py
@@ -5,7 +5,7 @@ from instagram_access_token import access_token
 def get_photos():
     photos = []
     api = InstagramAPI(access_token=access_token[0])
-    recent_media, next = api.user_recent_media(user_id=access_token[1]['id'], count=10)
+    recent_media, next = api.user_recent_media(user_id=access_token[1]['id'], count=5)
     for media in recent_media:
         caption_text = ''
         if media.caption is not None:
